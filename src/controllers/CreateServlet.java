0840d597f3e36ebb34501be38c7ec45f18341e33
tomoya.maita <maita.ws.tomoya@gmail.com>
2020-12-11 17:59:08 +0900

    modify Create/UpdateServlet.java

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -71,15 +71,6 @@ public class CreateServlet extends HttpServlet {
                 response.sendRedirect(request.getContextPath() + "/index");
             }
 
-
-
-            em.getTransaction().begin();
-            em.persist(m);
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "登録が完了しました。");
-            em.close();
-
-            response.sendRedirect(request.getContextPath() + "/index");
         }
     }
 
